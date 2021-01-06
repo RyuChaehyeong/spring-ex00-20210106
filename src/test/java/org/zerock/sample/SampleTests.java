289a1551d1defa7e94fbea1f9e664da55d1fc557
fluid <fluid@175.198.29.91>
2021-01-06 15:11:17 +0900

    ex00

---
+++ src/test/java/org/zerock/sample/SampleTests.java
@@ -0,0 +1,34 @@
+package org.zerock.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.log4j.Log4j;
+
+
+//@Log4j는 log라는 필드를 사용할 수 있게 자동으로 만들어준다.
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class SampleTests {
+	
+	@Autowired
+	private ApplicationContext context;
+	@Test
+	public void testExist() {
+		log.info("-------");
+		assertNotNull(context);
+		
+		//bean이 잘 있다고 알려줌
+		assertNotNull(context.getBean("chef"));
+		assertNotNull(context.getBean("restaurant"));
+	}
+
+}
